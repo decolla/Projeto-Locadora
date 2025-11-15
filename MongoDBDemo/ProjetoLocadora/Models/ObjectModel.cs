f1a0f60ac56f38ff229bd6e3103c9d7929e268b8
decolla <pedro.varise@gmail.com>
2025-11-15 16:47:12 -0300

    Back adicionado

---
+++ MongoDBDemo/ProjetoLocadora/Models/ObjectModel.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace ProjetoLocadora.Models;
+
+public class ObjectModel
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    public string TipoObjeto { get; set; }
+    public string NomeObjeto { get; set; }
+    public string AutorObjeto { get; set; }
+    public string AnoObjeto { get; set; }
+    public bool Disponivel { get; set; }
+
+}
